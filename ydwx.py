8bb3d9d729a0c6e678b2a5eb3d9b870f81baa87d
byhooi <byhooi@gmail.com>
2025-03-12 18:06:04 +0800

    fix

--- ydwx.py
+++ ydwx.py
@@ -26,7 +26,7 @@ for i in range(len(ydwx_deviceParams)):
     print(f'*****第{str(i+1)}个账号*****')
     timestamp = str(int(round(time.time() * 1000)))
     md5 = hashlib.md5()
-    sig = f'action=mixc.app.memberSign.sign&apiVersion=1.0&appId=68a91a5bac6a4f3e91bf4b42856785c6&appVersion=3.53.0&deviceParams={ydwx_deviceParams[i]}&imei=2333&mallNo=20014&osVersion=12.0.1&params=eyJtYWxsTm8iOiIyMDAxNCJ9&platform=h5&timestamp={timestamp}&token={ydwx_token[i]}&P@Gkbu0shTNHjhM!7F'
+    sig = f'action=mixc.app.memberSign.sign&apiVersion=1.0&appId=68a91a5bac6a4f3e91bf4b42856785c6&appVersion=3.53.0&deviceParams={ydwx_deviceParams[i]}&imei=2333&mallNo=8602A101&osVersion=12.0.1&params=eyJtYWxsTm8iOiIyMDAxNCJ9&platform=h5&timestamp={timestamp}&token={ydwx_token[i]}&P@Gkbu0shTNHjhM!7F'
     md5.update(sig.encode('utf-8'))
     sign = md5.hexdigest()
     url = 'https://app.mixcapp.com/mixc/gateway'
@@ -41,11 +41,11 @@ for i in range(len(ydwx_deviceParams)):
         'Content-Type': 'application/x-www-form-urlencoded',
         'X-Requested-With': 'com.crland.mixc',
         'Sec-Fetch-Site': 'same-origin',
-        'Referer': 'https://app.mixcapp.com/m/m-20014/signIn?showWebNavigation=true&timestamp=1676906528979&appVersion=3.53.0&mallNo=20014',
+        'Referer': 'https://app.mixcapp.com/m/m-8602A101/signIn?showWebNavigation=true&timestamp=1676906528979&appVersion=3.53.0&mallNo=8602A101',
         'Accept-Encoding': 'gzip, deflate',
         'Accept-Language': 'zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7'
     }
-    data = f'mallNo=20014&appId=68a91a5bac6a4f3e91bf4b42856785c6&platform=h5&imei=2333&appVersion=3.53.0&osVersion=12.0.1&action=mixc.app.memberSign.sign&apiVersion=1.0&timestamp={timestamp}&deviceParams={ydwx_deviceParams[i]}&token={ydwx_token[i]}&params=eyJtYWxsTm8iOiIyMDAxNCJ9&sign={sign}'
+    data = f'mallNo=8602A101&appId=68a91a5bac6a4f3e91bf4b42856785c6&platform=h5&imei=2333&appVersion=3.53.0&osVersion=12.0.1&action=mixc.app.memberSign.sign&apiVersion=1.0&timestamp={timestamp}&deviceParams={ydwx_deviceParams[i]}&token={ydwx_token[i]}&params=eyJtYWxsTm8iOiIyMDAxNCJ9&sign={sign}'
     html = requests.post(url=url, headers=headers, data=data)
     result = f'帐号{i+1}签到结果:' + json.loads(html.text)['message']
     print(result)
