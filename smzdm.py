68f9f71963960e3b8f4ee4894187e2f7b75e39a8
byhooi <byhooi@gmail.com>
2025-04-10 14:48:34 +0800

    refactor(smzdm.py): 重构签到脚本为面向对象结构
    将原有的脚本逻辑重构为面向对象的类结构，提升代码的可维护性和可读性。新增了 `SMZDM` 类，封装了签到、获取奖励等逻辑，并优化了环境变量的处理方式。

--- smzdm.py
+++ smzdm.py
@@ -1,64 +1,196 @@
-#!/usr/bin/python3
-# -- coding: utf-8 --
-# -------------------------------
-# @Author : github@wd210010 https://github.com/wd210010/just_for_happy
-# @Time : 2023/2/27 13:23
-# -------------------------------
-# cron "30 0 * * *" script-path=xxx.py,tag=匹配cron用
-# const $ = new Env('什么值得买签到')
+import hashlib
+import json
+import os
+import re
+import time
 
-import requests, json,time,hashlib,os
+import requests
+import urllib3
+
 from sendNotify import send
 
-# 青龙变量 zdm_cookie
-zdm_cookie = os.getenv("zdm_cookie").split('&')
+urllib3.disable_warnings()
 
-for i in range(len(zdm_cookie)):
-    print(f'开始第{i + 1}个帐号签到')
-    ts =int(round(time.time() * 1000))
-    url = 'https://user-api.smzdm.com/robot/token'
-    headers = {
-        'Host': 'user-api.smzdm.com',
-        'Content-Type': 'application/x-www-form-urlencoded',
-        'Cookie': f'{zdm_cookie[i]}',
-        'User-Agent': 'smzdm_android_V10.4.1 rv:841 (22021211RC;Android12;zh)smzdmapp',
-    }
-    data={
-        "f":"android",
-        "v":"10.4.1",
-        "weixin":1,
-        "time":ts,
-        "sign":hashlib.md5(bytes(f'f=android&time={ts}&v=10.4.1&weixin=1&key=apr1$AwP!wRRT$gJ/q.X24poeBInlUJC',encoding='utf-8')).hexdigest().upper()
-    }
-    html = requests.post(url=url, headers=headers, data=data)
-    result = html.json()
-    token= result['data']['token']
 
-    Timestamp =int(round(time.time() * 1000))
-    data={
-        "f":"android",
-        "v":"10.4.1",
-        "sk":"ierkM0OZZbsuBKLoAgQ6OJneLMXBQXmzX+LXkNTuKch8Ui2jGlahuFyWIzBiDq/L",
-        "weixin":1,
-        "time":Timestamp,
-        "token":token,
-        "sign":hashlib.md5(bytes(f'f=android&sk=ierkM0OZZbsuBKLoAgQ6OJneLMXBQXmzX+LXkNTuKch8Ui2jGlahuFyWIzBiDq/L&time={Timestamp}&token={token}&v=10.4.1&weixin=1&key=apr1$AwP!wRRT$gJ/q.X24poeBInlUJC',encoding='utf-8')).hexdigest().upper()
-    }
-    url = 'https://user-api.smzdm.com/checkin'
-    url2 = 'https://user-api.smzdm.com/checkin/all_reward'
-    headers = {
-        'Host': 'user-api.smzdm.com',
-        'Content-Type': 'application/x-www-form-urlencoded',
-        'Cookie': f'{zdm_cookie[i]}',
-        'User-Agent': 'smzdm_android_V10.4.1 rv:841 (22021211RC;Android12;zh)smzdmapp',
-    }
-    html = requests.post(url=url, headers=headers, data=data)
-    html2 = requests.post(url=url2, headers=headers, data=data)
-    result = json.loads(html.text)['error_msg']
-    result2 = json.loads(html2.text)
-    print(result)
-    print(result2)
-    
-    # 添加推送消息
-    msg = f'账号{i + 1}签到结果：\n{result}\n{result2}'
-    send('什么值得买签到', msg)
+class SMZDM:  # 移除 CheckIn 继承
+    name = "什么值得买"
+
+    def __init__(self):
+        self.check_item = {}
+        # 从环境变量获取 cookie
+        cookie = os.getenv('SMZDM.cookie')
+        if cookie:
+            self.check_item["cookie"] = cookie
+        else:
+            raise Exception("需要配置环境变量 SMZDM.cookie")
+
+    def robot_token(self, headers):
+        ts = int(round(time.time() * 1000))
+        url = "https://user-api.smzdm.com/robot/token"
+        data = {
+            "f": "android",
+            "v": "10.4.1",
+            "weixin": 1,
+            "time": ts,
+            "sign": hashlib.md5(
+                bytes(
+                    f"f=android&time={ts}&v=10.4.1&weixin=1&key=apr1$AwP!wRRT$gJ/q.X24poeBInlUJC",
+                    encoding="utf-8",
+                )
+            )
+            .hexdigest()
+            .upper(),
+        }
+        html = requests.post(url=url, headers=headers, data=data)
+        result = html.json()
+        token = result["data"]["token"]
+        return token
+
+    def sign(self, headers, token):
+        Timestamp = int(round(time.time() * 1000))
+        data = {
+            "f": "android",
+            "v": "10.4.1",
+            "sk": "ierkM0OZZbsuBKLoAgQ6OJneLMXBQXmzX+LXkNTuKch8Ui2jGlahuFyWIzBiDq/L",
+            "weixin": 1,
+            "time": Timestamp,
+            "token": token,
+            "sign": hashlib.md5(
+                bytes(
+                    f"f=android&sk=ierkM0OZZbsuBKLoAgQ6OJneLMXBQXmzX+LXkNTuKch8Ui2jGlahuFyWIzBiDq/L&time={Timestamp}&token={token}&v=10.4.1&weixin=1&key=apr1$AwP!wRRT$gJ/q.X24poeBInlUJC",
+                    encoding="utf-8",
+                )
+            )
+            .hexdigest()
+            .upper(),
+        }
+        url = "https://user-api.smzdm.com/checkin"
+        resp = requests.post(url=url, headers=headers, data=data)
+        error_msg = resp.json()["error_msg"]
+        return error_msg, data
+
+    def all_reward(self, headers, data):
+        url2 = "https://user-api.smzdm.com/checkin/all_reward"
+        resp = requests.post(url=url2, headers=headers, data=data)
+        result = resp.json()
+        msgs = []
+        if normal_reward := result["data"]["normal_reward"]:
+            msgs = [
+                {
+                    "name": "签到奖励",
+                    "value": normal_reward["reward_add"]["content"],
+                },
+                {
+                    "name": "连续签到",
+                    "value": normal_reward["sub_title"],
+                },
+            ]
+        return msgs
+
+    def active(self, cookie):
+        zdm_active_id = ["ljX8qVlEA7"]
+        for active_id in zdm_active_id:
+            url = f"https://zhiyou.smzdm.com/user/lottery/jsonp_draw?active_id={active_id}"
+            rewardurl = f"https://zhiyou.smzdm.com/user/lottery/jsonp_get_active_info?active_id={active_id}"
+            infourl = "https://zhiyou.smzdm.com/user/"
+            headers = {
+                "Host": "zhiyou.smzdm.com",
+                "Accept": "*/*",
+                "Connection": "keep-alive",
+                "Cookie": cookie,
+                "User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 15_6 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148/smzdm 10.4.6 rv:130.1 (iPhone 13; iOS 15.6; zh_CN)/iphone_smzdmapp/10.4.6/wkwebview/jsbv_1.0.0",
+                "Accept-Language": "zh-CN,zh-Hans;q=0.9",
+                "Referer": "https://m.smzdm.com/",
+                "Accept-Encoding": "gzip, deflate, br",
+            }
+            response = requests.post(url=url, headers=headers).json()
+            response_info = requests.get(url=infourl, headers=headers).text
+            _ = requests.get(url=rewardurl, headers=headers).json()
+            name = (
+                str(
+                    re.findall(
+                        '<a href="https://zhiyou.smzdm.com/user"> (.*?) </a>',
+                        str(response_info),
+                        re.S,
+                    )
+                )
+                .replace("[", "")
+                .replace("]", "")
+                .replace("'", "")
+            )
+            level = (
+                str(
+                    re.findall(
+                        r'<img src="https://res.smzdm.com/h5/h5_user/dist/assets/level/(.*?).png\?v=1">',
+                        str(response_info),
+                        re.S,
+                    )
+                )
+                .replace("[", "")
+                .replace("]", "")
+                .replace("'", "")
+            )
+            gold = (
+                str(
+                    re.findall(
+                        '<div class="assets-part assets-gold">\n                    (.*?)</span>',
+                        str(response_info),
+                        re.S,
+                    )
+                )
+                .replace("[", "")
+                .replace("]", "")
+                .replace("'’", "")
+                .replace('<span class="assets-part-element assets-num">', "")
+                .replace("'", "")
+            )
+            silver = (
+                str(
+                    re.findall(
+                        '<div class="assets-part assets-prestige">\n                    (.*?)</span>',
+                        str(response_info),
+                        re.S,
+                    )
+                )
+                .replace("[", "")
+                .replace("]", "")
+                .replace("'’", "")
+                .replace('<span class="assets-part-element assets-num">', "")
+                .replace("'", "")
+            )
+            msg = [
+                {
+                    "name": "签到结果",
+                    "value": response["error_msg"],
+                },
+                {"name": "等级", "value": level},
+                {"name": "昵称", "value": name},
+                {"name": "金币", "value": gold},
+                {"name": "碎银", "value": silver},
+            ]
+        return msg
+
+    def main(self):
+        cookie = self.check_item.get("cookie")
+        headers = {
+            "Host": "user-api.smzdm.com",
+            "Content-Type": "application/x-www-form-urlencoded",
+            "Cookie": cookie,
+            "User-Agent": "smzdm_android_V10.4.1 rv:841 (22021211RC;Android12;zh)smzdmapp",
+        }
+        msg = self.active(cookie)
+        token = self.robot_token(headers)
+        error_msg, data = self.sign(headers, token)
+        msg.append({"name": "签到结果", "value": error_msg})
+        reward_msg = self.all_reward(headers, data)
+        msg += reward_msg
+        msg = "\n".join([f"{one.get('name')}: {one.get('value')}" for one in msg])
+        
+        # 添加推送通知
+        send('什么值得买签到', msg)
+        return msg
+
+
+if __name__ == "__main__":
+    smzdm = SMZDM()
+    print(smzdm.main())
