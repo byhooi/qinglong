9e2ea5abccc191329c58dc030bc1c09944f9ec4a
byhooi <byhooi@gmail.com>
2025-04-11 08:11:09 +0800

    fix

--- smzdm.py
+++ smzdm.py
@@ -74,8 +74,90 @@ class SMZDM:
         resp = requests.post(url=url2, headers=headers, data=data)
         result = resp.json()
         msgs = []
-        # 由于签到奖励信息结构变化，我们直接返回空列表，避免显示未知奖励
         return msgs
+
+    def active(self, cookie):
+        zdm_active_id = ["ljX8qVlEA7"]
+        for active_id in zdm_active_id:
+            url = f"https://zhiyou.smzdm.com/user/lottery/jsonp_draw?active_id={active_id}"
+            rewardurl = f"https://zhiyou.smzdm.com/user/lottery/jsonp_get_active_info?active_id={active_id}"
+            infourl = "https://zhiyou.smzdm.com/user/"
+            headers = {
+                "Host": "zhiyou.smzdm.com",
+                "Accept": "*/*",
+                "Connection": "keep-alive",
+                "Cookie": cookie,
+                "User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 15_6 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148/smzdm 10.4.6 rv:130.1 (iPhone 13; iOS 15.6; zh_CN)/iphone_smzdmapp/10.4.6/wkwebview/jsbv_1.0.0",
+                "Accept-Language": "zh-CN,zh-Hans;q=0.9",
+                "Referer": "https://m.smzdm.com/",
+                "Accept-Encoding": "gzip, deflate, br",
+            }
+            response = requests.post(url=url, headers=headers).json()
+            response_info = requests.get(url=infourl, headers=headers).text
+            _ = requests.get(url=rewardurl, headers=headers).json()
+            name = (
+                str(
+                    re.findall(
+                        '<a href="https://zhiyou.smzdm.com/user"> (.*?) </a>',
+                        str(response_info),
+                        re.S,
+                    )
+                )
+                .replace("[", "")
+                .replace("]", "")
+                .replace("'", "")
+            )
+            level = (
+                str(
+                    re.findall(
+                        r'<img src="https://res.smzdm.com/h5/h5_user/dist/assets/level/(.*?).png\?v=1">',
+                        str(response_info),
+                        re.S,
+                    )
+                )
+                .replace("[", "")
+                .replace("]", "")
+                .replace("'", "")
+            )
+            gold = (
+                str(
+                    re.findall(
+                        '<div class="assets-part assets-gold">\n                    (.*?)</span>',
+                        str(response_info),
+                        re.S,
+                    )
+                )
+                .replace("[", "")
+                .replace("]", "")
+                .replace("''", "")
+                .replace('<span class="assets-part-element assets-num">', "")
+                .replace("'", "")
+            )
+            silver = (
+                str(
+                    re.findall(
+                        '<div class="assets-part assets-prestige">\n                    (.*?)</span>',
+                        str(response_info),
+                        re.S,
+                    )
+                )
+                .replace("[", "")
+                .replace("]", "")
+                .replace("''", "")
+                .replace('<span class="assets-part-element assets-num">', "")
+                .replace("'", "")
+            )
+            msg = [
+                {
+                    "name": "签到结果",
+                    "value": response["error_msg"],
+                },
+                {"name": "等级", "value": level},
+                {"name": "昵称", "value": name},
+                {"name": "金币", "value": gold},
+                {"name": "碎银", "value": silver},
+            ]
+        return msg
 
     def main(self):
         cookie = self.check_item.get("cookie")
